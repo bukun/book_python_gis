63adced3c4c08f552370729ed826c03b644538ad
bukun <bukun@osgeo.cn>
2016-12-07 10:40:50 +0800

    foo.

---
+++ script_gen_sh.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+fo = open('xx_run.sh', 'w')
+cws = os.getcwd()
+for wroot, wdirs, wfiles in os.walk(cws):
+    for wfile in wfiles:
+        if wfile.endswith('.py'):
+            if wfile == '__init__.py':
+                continue
+            pyfile = os.path.join(wroot, wfile)[len(cws) + 1:]
+            print(pyfile)
+
+            uu = pyfile[:-3].split('/')
+            print(uu)
+            if len(uu) > 1:
+                outstr = '.'.join(uu)
+                fo.write( 'python3 -m {0}\n'.format(outstr))
+
+fo.close()
